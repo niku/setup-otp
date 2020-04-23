14007f2dca1104f35c1520694fa26d7d094c6060
niku <niku@niku.name>
2020-04-24 00:05:24 +0900

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { getInput, setFailed } from "@actions/core";
+import { getInput, debug, setFailed } from "@actions/core";
 import { installOTP } from "./install-otp";
 
 async function run(): Promise<void> {
   try {
     const otpVersion = getInput("otp-version");
+    debug(`otp-version: ${otpVersion}`);
     await installOTP(otpVersion);
   } catch (error) {
     setFailed(error.message);
